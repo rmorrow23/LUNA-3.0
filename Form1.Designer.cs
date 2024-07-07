f17e3f38e8bd8f15f9f86ee09a8b564854f56339
Randal Morrow <randalmorrow24@gmail.com>
2024-07-07 18:59:22 -0400

    UPLOAD

---
+++ Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace Moonbase
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.GRBXLocationInfo = new System.Windows.Forms.GroupBox();
+            this.LBLRoomDesc = new System.Windows.Forms.Label();
+            this.TBRoomName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.personalNav = new System.Windows.Forms.GroupBox();
+            this.LobbyBTN = new System.Windows.Forms.Button();
+            this.medBayBTN = new System.Windows.Forms.Button();
+            this.commandCenterBTN = new System.Windows.Forms.Button();
+            this.livingQtBTN = new System.Windows.Forms.Button();
+            this.labBTN = new System.Windows.Forms.Button();
+            this.TXBXRoomDesc = new System.Windows.Forms.TextBox();
+            this.GRBXLocationInfo.SuspendLayout();
+            this.personalNav.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // GRBXLocationInfo
+            // 
+            this.GRBXLocationInfo.BackColor = System.Drawing.Color.Transparent;
+            this.GRBXLocationInfo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.GRBXLocationInfo.Controls.Add(this.label1);
+            this.GRBXLocationInfo.Controls.Add(this.LBLRoomDesc);
+            this.GRBXLocationInfo.Controls.Add(this.TXBXRoomDesc);
+            this.GRBXLocationInfo.Controls.Add(this.TBRoomName);
+            this.GRBXLocationInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GRBXLocationInfo.ForeColor = System.Drawing.Color.White;
+            this.GRBXLocationInfo.Location = new System.Drawing.Point(11, 12);
+            this.GRBXLocationInfo.Margin = new System.Windows.Forms.Padding(2);
+            this.GRBXLocationInfo.Name = "GRBXLocationInfo";
+            this.GRBXLocationInfo.Padding = new System.Windows.Forms.Padding(2);
+            this.GRBXLocationInfo.Size = new System.Drawing.Size(332, 431);
+            this.GRBXLocationInfo.TabIndex = 0;
+            this.GRBXLocationInfo.TabStop = false;
+            this.GRBXLocationInfo.Text = "Location Information";
+            // 
+            // LBLRoomDesc
+            // 
+            this.LBLRoomDesc.AutoSize = true;
+            this.LBLRoomDesc.BackColor = System.Drawing.Color.Silver;
+            this.LBLRoomDesc.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LBLRoomDesc.ForeColor = System.Drawing.Color.Black;
+            this.LBLRoomDesc.Location = new System.Drawing.Point(12, 152);
+            this.LBLRoomDesc.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.LBLRoomDesc.Name = "LBLRoomDesc";
+            this.LBLRoomDesc.Size = new System.Drawing.Size(206, 29);
+            this.LBLRoomDesc.TabIndex = 2;
+            this.LBLRoomDesc.Text = "Room Description";
+            this.LBLRoomDesc.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // TBRoomName
+            // 
+            this.TBRoomName.BackColor = System.Drawing.Color.Silver;
+            this.TBRoomName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TBRoomName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TBRoomName.Location = new System.Drawing.Point(12, 84);
+            this.TBRoomName.Margin = new System.Windows.Forms.Padding(2);
+            this.TBRoomName.Name = "TBRoomName";
+            this.TBRoomName.ReadOnly = true;
+            this.TBRoomName.Size = new System.Drawing.Size(300, 19);
+            this.TBRoomName.TabIndex = 1;
+            this.TBRoomName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.TBRoomName.TextChanged += new System.EventHandler(this.TBRoomName_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Silver;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(12, 60);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Room Name";
+            // 
+            // personalNav
+            // 
+            this.personalNav.BackColor = System.Drawing.Color.Transparent;
+            this.personalNav.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.personalNav.Controls.Add(this.LobbyBTN);
+            this.personalNav.Controls.Add(this.medBayBTN);
+            this.personalNav.Controls.Add(this.commandCenterBTN);
+            this.personalNav.Controls.Add(this.livingQtBTN);
+            this.personalNav.Controls.Add(this.labBTN);
+            this.personalNav.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.personalNav.ForeColor = System.Drawing.SystemColors.Info;
+            this.personalNav.Location = new System.Drawing.Point(405, 12);
+            this.personalNav.Name = "personalNav";
+            this.personalNav.Size = new System.Drawing.Size(385, 197);
+            this.personalNav.TabIndex = 1;
+            this.personalNav.TabStop = false;
+            this.personalNav.Text = "Personal Nav Device";
+            // 
+            // LobbyBTN
+            // 
+            this.LobbyBTN.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LobbyBTN.ForeColor = System.Drawing.Color.Black;
+            this.LobbyBTN.Image = global::Moonbase.Properties.Resources.buttonBackground;
+            this.LobbyBTN.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.LobbyBTN.Location = new System.Drawing.Point(134, 79);
+            this.LobbyBTN.Name = "LobbyBTN";
+            this.LobbyBTN.Size = new System.Drawing.Size(99, 63);
+            this.LobbyBTN.TabIndex = 4;
+            this.LobbyBTN.Text = "Lobby";
+            this.LobbyBTN.UseVisualStyleBackColor = true;
+            this.LobbyBTN.Click += new System.EventHandler(this.LobbyBTN_Click);
+            // 
+            // medBayBTN
+            // 
+            this.medBayBTN.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.medBayBTN.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.medBayBTN.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.medBayBTN.Image = global::Moonbase.Properties.Resources.buttonBackground;
+            this.medBayBTN.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.medBayBTN.Location = new System.Drawing.Point(120, 148);
+            this.medBayBTN.Name = "medBayBTN";
+            this.medBayBTN.Size = new System.Drawing.Size(129, 40);
+            this.medBayBTN.TabIndex = 3;
+            this.medBayBTN.Text = "Medbay";
+            this.medBayBTN.UseVisualStyleBackColor = true;
+            this.medBayBTN.Click += new System.EventHandler(this.medBayBTN_Click);
+            // 
+            // commandCenterBTN
+            // 
+            this.commandCenterBTN.BackColor = System.Drawing.Color.Transparent;
+            this.commandCenterBTN.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.commandCenterBTN.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.commandCenterBTN.ForeColor = System.Drawing.Color.Black;
+            this.commandCenterBTN.Image = global::Moonbase.Properties.Resources.buttonBackground;
+            this.commandCenterBTN.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.commandCenterBTN.Location = new System.Drawing.Point(120, 30);
+            this.commandCenterBTN.Name = "commandCenterBTN";
+            this.commandCenterBTN.Size = new System.Drawing.Size(129, 38);
+            this.commandCenterBTN.TabIndex = 0;
+            this.commandCenterBTN.Text = "Command";
+            this.commandCenterBTN.UseVisualStyleBackColor = false;
+            this.commandCenterBTN.Click += new System.EventHandler(this.commandCenterBTN_Click);
+            // 
+            // livingQtBTN
+            // 
+            this.livingQtBTN.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.livingQtBTN.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.livingQtBTN.ForeColor = System.Drawing.Color.Black;
+            this.livingQtBTN.Image = global::Moonbase.Properties.Resources.buttonBackground;
+            this.livingQtBTN.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.livingQtBTN.Location = new System.Drawing.Point(239, 89);
+            this.livingQtBTN.Name = "livingQtBTN";
+            this.livingQtBTN.Size = new System.Drawing.Size(103, 42);
+            this.livingQtBTN.TabIndex = 2;
+            this.livingQtBTN.Text = "Living";
+            this.livingQtBTN.UseVisualStyleBackColor = true;
+            this.livingQtBTN.Click += new System.EventHandler(this.livingQtBTN_Click);
+            // 
+            // labBTN
+            // 
+            this.labBTN.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.labBTN.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.labBTN.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labBTN.ForeColor = System.Drawing.Color.Black;
+            this.labBTN.Image = global::Moonbase.Properties.Resources.buttonBackground;
+            this.labBTN.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.labBTN.Location = new System.Drawing.Point(33, 89);
+            this.labBTN.Name = "labBTN";
+            this.labBTN.Size = new System.Drawing.Size(95, 42);
+            this.labBTN.TabIndex = 1;
+            this.labBTN.Text = "Lab";
+            this.labBTN.UseVisualStyleBackColor = true;
+            this.labBTN.Click += new System.EventHandler(this.labBTN_Click);
+            // 
+            // TXBXRoomDesc
+            // 
+            this.TXBXRoomDesc.BackColor = System.Drawing.Color.Silver;
+            this.TXBXRoomDesc.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TXBXRoomDesc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TXBXRoomDesc.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.TXBXRoomDesc.Location = new System.Drawing.Point(12, 177);
+            this.TXBXRoomDesc.Margin = new System.Windows.Forms.Padding(2);
+            this.TXBXRoomDesc.Multiline = true;
+            this.TXBXRoomDesc.Name = "TXBXRoomDesc";
+            this.TXBXRoomDesc.ReadOnly = true;
+            this.TXBXRoomDesc.Size = new System.Drawing.Size(306, 241);
+            this.TXBXRoomDesc.TabIndex = 3;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.BackgroundImage = global::Moonbase.Properties.Resources.lobby;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(802, 458);
+            this.Controls.Add(this.personalNav);
+            this.Controls.Add(this.GRBXLocationInfo);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LUNA";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.GRBXLocationInfo.ResumeLayout(false);
+            this.GRBXLocationInfo.PerformLayout();
+            this.personalNav.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox GRBXLocationInfo;
+        private System.Windows.Forms.TextBox TBRoomName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label LBLRoomDesc;
+        private System.Windows.Forms.GroupBox personalNav;
+        private System.Windows.Forms.Button medBayBTN;
+        private System.Windows.Forms.Button livingQtBTN;
+        private System.Windows.Forms.Button labBTN;
+        private System.Windows.Forms.Button commandCenterBTN;
+        private System.Windows.Forms.Button LobbyBTN;
+        private System.Windows.Forms.TextBox TXBXRoomDesc;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,180 @@
+﻿using Moonbase.Properties;
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Moonbase
+{
+    public partial class FormMain : Form
+    {
+        // Variable to hold the user's current position
+        string currentPosition;
+        // array to hold different locations
+        string[] location = new string [5];
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        //function to update form based on location
+        public void updateForm(string newLocation)
+        {   //checks the new location selected to see what info to put
+            if (newLocation == location[0])
+            {
+                // Update user position
+                UpdateUserPosition("Command Center");
+                
+                // Change the background image to command center
+                this.BackgroundImage = Properties.Resources.command_center;
+                // Change Room Title to Command Center
+                TBRoomName.Text = "Lunar Operations Command Center";
+                // Change room description to Command Center Description
+                TXBXRoomDesc.Text = "The Lunar Operations Command Center is the heart of the moonbase, featuring walls of sleek metallic panels embedded with soft, ambient LED lights that create a futuristic glow. Large, reinforced windows offer a breathtaking view of the lunar landscape and Earth. High-tech consoles with holographic displays dominate the room, providing real-time data and interactive controls for managing base operations.";
+            }
+            else if (newLocation == location[1])
+            {
+                // Update user position
+                UpdateUserPosition("Lab");
+                // Change background image to Laboratory
+                this.BackgroundImage = Properties.Resources.lab;
+                // Change Room Title to Laboratory
+                TBRoomName.Text = "Lunar Laboratory";
+                // Change room description to Laboratory Description
+                TXBXRoomDesc.Text = "The Lunar Laboratory is a hub of scientific discovery, featuring sleek metallic walls illuminated by ambient LED lights. Large windows offer a view of the lunar surface, enhancing the high-tech atmosphere. Equipped with advanced scientific instruments and robotic assistants, the lab enables groundbreaking experiments and efficient research.";
+            }
+
+            else if(newLocation == location[2])
+            {
+                // Update user position
+                UpdateUserPosition("Living Quarters");
+                // Change background image to Living Quarters
+                this.BackgroundImage = Properties.Resources.living;
+                // Change Room Title to Living Quarters
+                TBRoomName.Text = "Lunar Living Quarters";
+                // Change room description to Living Quarters Description
+                TXBXRoomDesc.Text = "The Lunar Living Quarters offer a cozy refuge with sleek metallic walls and warm LED lighting. Large windows provide a stunning lunar landscape view. Modern furniture, including a comfortable bed, seating area, and holographic work desk, ensure comfort and functionality, while personal items add a homely touch.";
+            }
+
+            else if(newLocation == location[3])
+            {
+                // Update user position
+                UpdateUserPosition("MedBay");
+                // Change background image to MedBay
+                this.BackgroundImage = Properties.Resources.medbay;
+                // Change Room Title to MedBay
+                TBRoomName.Text = "Lunar Medical Bay";
+                // Change room description to MedBay Description
+                TXBXRoomDesc.Text = "The Lunar Medbay is a state-of-the-art medical facility with sleek metallic walls and soft LED lighting. Large windows provide a calming view of the lunar surface. Equipped with advanced medical technology and robotic assistants, the medbay ensures top-notch care and efficient patient management.";
+            }
+
+            else if (newLocation == location[4])
+            {
+                // Update user position
+                UpdateUserPosition("Lobby");
+                // Change background image to MedBay
+                this.BackgroundImage = Properties.Resources.lobby;
+                // Change Room Title to MedBay
+                TBRoomName.Text = "Lunar Lobby";
+                // Change room description to MedBay Description
+                TXBXRoomDesc.Text = "The Lunar Lobby is the central hub of the moonbase, featuring high ceilings, sleek metallic walls, and large panoramic windows offering stunning lunar views. Modern seating areas and a state-of-the-art reception desk with holographic displays provide comfort and efficient communication. Astronauts in advanced space suits and robotic assistants seamlessly move through the lobby, reflecting the integration of human and robotic efforts in moonbase operations.";
+            }
+            disableNav(newLocation);
+        }
+
+        public void disableNav(string currentlocation)
+        {
+            if ((currentlocation == location[0]) || (currentlocation == location[1]) || (currentlocation == location[2]) || (currentlocation == location[3]))
+            {
+                //sets all buttons to disabled except the main lobby
+                commandCenterBTN.Enabled = false;
+                labBTN.Enabled = false;
+                livingQtBTN.Enabled = false;
+                medBayBTN.Enabled = false;
+
+                LobbyBTN.Enabled = true;
+            }
+
+            else if (currentlocation == location[4])
+            {
+                //re-enable all buttons except the main lobby
+                commandCenterBTN.Enabled = true;
+                labBTN.Enabled = true;
+                livingQtBTN.Enabled = true;
+                medBayBTN.Enabled = true;
+
+                LobbyBTN.Enabled = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Initialize array with the different locations
+            location[0] = "Command Center";
+            location[1] = "Laboratory";
+            location[2] = "Living Quarters";
+            location[3] = "Medical Bay";
+            location[4] = "Lobby";
+
+            updateForm(location[4]);
+        }
+
+        private void UpdateUserPosition(string newPosition)
+        {
+            currentPosition = newPosition;
+            LogUserPosition();
+        }
+
+        private void LogUserPosition()
+        {
+            //log file will be under MoonBase\bin\x64\Debug
+            string logFilePath = "user_location_log.txt";
+            string logMessage = $"{DateTime.Now}: User moved to {currentPosition}";
+
+            try
+            {   //saves the new location to the specified log file
+                File.AppendAllText(logFilePath, logMessage + Environment.NewLine);
+            }
+            catch (Exception ex)
+            {   //shows error message
+                MessageBox.Show($"Error logging user position: {ex.Message}");
+            }
+        }
+
+        private void TBRoomName_TextChanged(object sender, EventArgs e)
+        {
+            // Handle any changes to the room name textbox if necessary
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            // Handle any actions related to label2 if necessary
+        }
+
+        // Nav Buttons
+        private void commandCenterBTN_Click(object sender, EventArgs e)
+        {
+            updateForm(location[0]);
+        }
+
+        private void labBTN_Click(object sender, EventArgs e)
+        {
+            updateForm(location[1]);
+        }
+
+        private void medBayBTN_Click(object sender, EventArgs e)
+        {
+            updateForm(location[3]);
+        }
+
+        private void livingQtBTN_Click(object sender, EventArgs e)
+        {
+            updateForm(location[2]);
+        }
+
+        private void LobbyBTN_Click(object sender, EventArgs e)
+        {
+            updateForm(location[4]);
+        }
+    }
+}
